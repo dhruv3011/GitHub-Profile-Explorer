988f0c9a465e6d71105b3413717dbf9e12e8c59c
Dhruv Vaishnav <dhruv3011@gmail.com>
2026-03-19 12:26:17 -0400

    Changes for MainActivity

--- app/src/main/java/com/dhruv/githubprofileexplorer/MainActivity.kt
+++ app/src/main/java/com/dhruv/githubprofileexplorer/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                 Surface(
